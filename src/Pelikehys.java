209f7e261abb3ff4a45a3f666e7e63b6499af938
Heikki Ahokas <heikki.ahokas@esedulainen.fi>
2021-05-11 12:52:24 +0300

    peli-ikkunan korkeus 950

--- src/Pelikehys.java
+++ src/Pelikehys.java
@@ -22,7 +22,7 @@ public class Pelikehys implements Runnable {
         kehys.setTitle("Laivapeli");
         kehys.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
         kehys.setLayout(null); //Nyt voi asetella käsin JPaneleita yms. kehykseen, kun tuo on nollattu.
-        kehys.setSize(1600, 800);
+        kehys.setSize(1600, 950);
         kehys.add(kentta);
         kehys.revalidate();
         kehys.add(laskinikkuna.getLaskinikkuna()); //Ei tuntunut toimivan pelkästään olio lisäämällä. Ainakin getterillä toimii.
