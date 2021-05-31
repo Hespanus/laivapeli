ab1a767d8df8c56491e9cf23ea36f66f6dd7c967
Saimiii <nasutiusanen@gmail.com>
2021-05-31 11:05:29 +0300

    tekstikenttä ja ammu-nappula lisätty

--- src/Alaruudukko.java
+++ src/Alaruudukko.java
@@ -6,28 +6,29 @@ import javax.swing.*;
 public class Alaruudukko {
 
     public JPanel alaruudukko;
-    //public JTextArea tekstikentta;    
-    //public JButton ammu;
+    public JTextField tekstikentta;
+
 
     public Alaruudukko(){
 
         this.alaruudukko = new JPanel();
         this.alaruudukko.setBounds(2, 603, 1200, 302);
         this.alaruudukko.setBackground(Color.white);
+        JButton nappula = new JButton ("AMMU");
 
-        //tekstikentta = new JTextArea();
-       // alaruudukko.add(tekstikentta);
-        //ammu = new JButton("AMMU");
+        nappula.setBounds(200, 610, 100, 50);
+        alaruudukko.add(nappula);
+       
 
-        
-
-        //alaruudukko.add(ammu);
-
+        JTextField tekstikentta;
+        tekstikentta = new JTextField("Ammu laiva!");
+        tekstikentta.setBounds(2, 1000, 100, 200);
+        alaruudukko.add(tekstikentta);
+      
+ 
     }
         public JPanel getAlaruudukko(){
             return this.alaruudukko;
         }
-        //public JTextArea gettekstikentta(){
-          //  return this.tekstikentta;
-    //}
-}
+      
+    }
